b535f746993646dbfeb052ed856fffb89a21e270
gowthamtn37 <gowthamtn37@gmail.com>
2023-02-05 12:30:46 +0530

    crud-post

--- src/Addmovie.jsx
+++ src/Addmovie.jsx
@@ -2,14 +2,36 @@ import React from 'react';
 import { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import { useNavigate } from 'react-router-dom';
 
-export function Addmovie({ movielist, setMovielist }) {
+export function Addmovie() {
 
   const [title, setTitle] = useState("");
   const [pic, setPic] = useState("");
   const [des, setDes] = useState("");
   const [rat, setRat] = useState("");
-  const [tra, setTrailer] = useState("");
+  const [tra, setTrailer] = useState(""); 
+
+const navigate = useNavigate();
+
+  const addMovie = async() => {
+    const newMovie = { 
+      name: title,
+      poster: pic,
+      summary: des,
+      rating: rat,
+      trailer: tra,
+    };
+
+    console.log(newMovie);
+
+    await fetch("https://63d75fd5afbba6b7c93bed33.mockapi.io/movies",
+    { method:'POST',
+     body: JSON.stringify(newMovie), 
+     headers:{"Content-Type": "application/json",},
+    });
+    navigate('/movies')
+  } 
 
   return (
     <div>
@@ -26,17 +48,7 @@ export function Addmovie({ movielist, setMovielist }) {
             <input type='text' placeholder='Summary' onChange={(event) => setDes(event.target.value)}></input>
             <input type='text' placeholder='rating' onChange={(event) => setRat(event.target.value)}></input> */}
 
-        <Button variant="contained" color='success' type='submit' onClick={() => {
-          const newMovie = {
-            name: title,
-            poster: pic,
-            summary: des,
-            rating: rat,
-            trailer: tra,
-          };
-          setMovielist([...movielist, newMovie]);
-
-        }}>ADD Movie</Button>
+        <Button variant="contained" color='success' type='button' onClick={addMovie}>ADD Movie</Button>
       </div><br />
     </div>
   );
